8a5df158acd3c1d826743c7dfae8bb7a200d274c
Ankit Haldar <ankithaldar1995@gmail.com>
2019-01-21 16:54:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -101,10 +101,8 @@ module.exports = function arrayToTree(data, options) {
 
   var grouped = groupByParents(deepClone(data), options);
 
-  Object.keys(grouped).map(x => grouped[x] = grouped[x].sort((a, b) => a.position > b.position))
+  Object.keys(grouped).map(x => grouped[x] = grouped[x].sort((a, b) => parseFloat(a.position) > parseFloat(b.position)))
   
-  console.log("HAHAHAHAHAH", JSON.stringify(grouped))
-
   return createTree(
     grouped,
     grouped[options.rootID],
